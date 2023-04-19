b1dd394c8a7636db7044664bdd98190c4256112f
Yata <freyflorian727@protonmail.com>
2023-04-19 17:18:06 +0200

    Initial commit

--- src/internalJavaEasy/II_KaraTheTunnelSeeker.java
+++ src/internalJavaEasy/II_KaraTheTunnelSeeker.java
@@ -1,5 +1,9 @@
 package internalJavaEasy;
 import javakara.JavaKaraProgram;
 public class II_KaraTheTunnelSeeker extends JavaKaraProgram {
-
+	public void myProgram() {
+		while (!kara.treeLeft() || !kara.treeRight()) {
+			kara.move();
+		}
+	}
 }
--- src/internalJavaEasy/IV_CloverleafSearchInForest.java
+++ src/internalJavaEasy/IV_CloverleafSearchInForest.java
@@ -1,5 +1,23 @@
 package internalJavaEasy;
 import javakara.JavaKaraProgram;
 public class IV_CloverleafSearchInForest extends JavaKaraProgram {
-
+	void treeBypass() {
+		kara.turnLeft();
+		kara.move();
+		kara.turnRight();
+		kara.move();
+		kara.move();
+		kara.turnRight();
+		kara.move();
+		kara.turnLeft();
+	}
+	public void myProgram() {
+		while (!kara.onLeaf()) {
+			if (!kara.treeFront()) {
+				kara.move();
+			} else {
+				this.treeBypass();
+			}
+		}
+	}
 }
