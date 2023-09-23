ee95b407eb355a788468ff1612e10ebdb0913538
Yata <freyflorian727@protonmail.com>
2023-09-23 16:21:01 +0200

    Initial commit

--- src/advancedRecursion/inFrontX.java
+++ src/advancedRecursion/inFrontX.java
@@ -2,23 +2,26 @@ package advancedRecursion;
 
 import javakara.JavaKaraProgram;
 
-public class moveX extends JavaKaraProgram {
+public class inFrontX extends JavaKaraProgram {
 
     public static void main(String[] args) {
-        new moveX().run();
+        new inFrontX().run();
     }
 
     public void myMainProgram() {
         kara.setPosition(0, 0);
-        this.start(4);
+        this.start(10, true);
     }
 
-    int start(int n) {
+    int start(int n, boolean leaf) {
         if (n < 1) {
             return 0;
         } else {
+            if (leaf) {
+                kara.putLeaf();
+            }
             kara.move();
-            return start(n - 1);
+            return start(n - 1, leaf);
         }
     }
 }
---
+++ src/advancedRecursion/spiral.java
@@ -0,0 +1,53 @@
+package advancedRecursion;
+
+import javakara.JavaKaraProgram;
+
+public class spiral extends JavaKaraProgram {
+
+    public static void main(String[] args) {
+        new spiral().run();
+    }
+
+    public void myMainProgram() {
+        world.setSize(500, 500);
+        kara.setPosition(250, 250);
+        this.start(1, true);
+    }
+
+    int start(int n, boolean leaf) {
+        if (n > 99) {
+            kara.turnLeft();
+            kara.turnLeft();
+        } else {
+            if (leaf) {
+                this.putX(n);
+            } else {
+                this.moveX(n);
+            }
+            kara.turnLeft();
+            this.start(n + 1, leaf);
+            kara.turnRight();
+            this.moveX(n);
+        }
+        return 0;
+    }
+
+    int moveX(int n) {
+        if (n < 1) {
+            return 0;
+        } else {
+            kara.move();
+            return this.moveX(n - 1);
+        }
+    }
+
+    int putX(int n) {
+        if (n < 1) {
+            return 0;
+        } else {
+            kara.putLeaf();
+            kara.move();
+            return this.putX(n - 1);
+        }
+    }
+}
